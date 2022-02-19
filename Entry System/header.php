38776571c621a5936259a949f585aefccf07b584
Hanumath venkata rama Harshith Emani <comewalkwithmecreations@gmail.com>
2022-02-19 12:19:54 -0500

    Add files via upload

---
+++ Entry System/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <title>CS684</title>
+  <link rel="stylesheet" href="animate.css">
+    <script src = "xml.js"></script>
+
+    <video id="background-video" autoplay loop muted poster="black.jpg">
+        <source src="worldsv.mp4" type="video/mp4">
+      </video>
+
+      <nav>
+		<div class="logo">
+			<p>DEATHEATERS</p>
+		</div>
+		<ul>
+			<li><a href="index.php" class="active">HOME</a></li>
+			<li><a href="api.html">CALL  API</a></li>
+			<li><a href="aboutus.html">ABOUT US</a></li>
+      <li><a href="signup.php">signup</a></li>
+      <li><a href="login.php">login</a></li>
+		</ul>
+	</nav>
+
+  <div class="wrapper">
+
+
+    <style>
+        h1 {text-align: center; color: white;line-height: 700px;}
+    </style>
---
+++ Entry System/login.php
@@ -0,0 +1,27 @@
+<?php
+ include_once 'header.php';
+ ?>
+
+<link rel="stylesheet" href="animate.css">
+
+
+<section class="login-form">
+<h1>login</h1>
+<div class="login-form-form">
+<form action="login.inc.php" method="post">
+
+<input type="text" name="uid" placeholder="username...">
+<input type="password" name="pwd" placeholder="password...">
+
+<button type="submit" name="submit">login</button>
+</form>
+</div>
+</section>
+
+
+
+
+
+<?php
+ include_once 'footer.php';
+ ?>=
