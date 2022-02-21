2061118e58a9807bf0e52186ffc6b69ea6518b3d
TanishBugnait29 <81473377+TanishBugnait29@users.noreply.github.com>
2022-02-20 23:53:41 -0500

    Update signup.php

--- Entry System/signup.php
+++ Entry System/signup.php
@@ -2,11 +2,8 @@
  include_once 'header.php';
  ?>
 
-
-
 <section class="signup-form">
-<h1>sign up</h1>
-
+<h2>sign up</h2>
 <div class="signup-form-form">
 <form action="include/signup.inc.php" method="post">
 <input type="text" name="name" placeholder="Full name...">
@@ -17,7 +14,35 @@
 <button type="submit" name="submit">sign up</button>
 </form>
 </div>
+<?php
+if (isset($_GET["error"])){
+     if ($_GET["error"] == "stmt failed"){
+        echo "<p>Fill in all fields</p>";
+    }
+
+else if ($_GET["error"] == "invaliduid"){
+    echo "<p>Choose a proper username!</p>";
+}
+else if ($_GET["error"] == "invalidemail"){
+    echo "<p>Choose a proper email!</p>";
+}
+else if ($_GET["error"] == "password doesnot match"){
+    echo "<p>Make sure the passowrds match!</p>";
+}
+else if ($_GET["error"] == "stmt failed"){
+    echo "<p>Something went wrong , try again!</p>";
+}
+else if ($_GET["error"] == "username taken"){
+    echo "<p>This username has already taken!</p>";
+}
+else if ($_GET["error"] == "none"){
+    echo "<p>Signed up!</p>";
+  }
+}
+?> 
 </section>
+
+}
 
 <?php
  include_once 'footer.php';
